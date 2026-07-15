dbb18e7750807cba0638b2b5307a4bec919f8cd7
deng.qb <1093586934@qq.com>
2026-07-15 14:35:06 +0800

    fix(service): 修复预推送风险演示服务中的变量初始化值
    - 将变量 i 的初始值从 3 更改为 4

--- codeperf-demo/src/main/java/com/codeperf/demo/service/PrePushRiskDemoService.java
+++ codeperf-demo/src/main/java/com/codeperf/demo/service/PrePushRiskDemoService.java
@@ -13,7 +13,7 @@ public class PrePushRiskDemoService {
 
     public PrePushRiskDemoService(OrderMapper orderMapper, DeliveryClient deliveryClient) {
         this.orderMapper = orderMapper;
-        int i = 3;
+        int i = 4;
         this.deliveryClient = deliveryClient;
     }
 
